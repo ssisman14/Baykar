e415fc29577377bfaf3b84de3b38e8b842be5ccb
Selahattin Şişman <s.sisman06@gmail.com>
2022-07-21 23:55:28 +0300

    Update settings.py

--- Baykar/settings.py
+++ Baykar/settings.py
@@ -154,3 +154,11 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'Your email user'
+EMAIL_HOST_PASSWORD = "YOUR EMAIL PASSWORD"
+EMAIL_USE_TLS = True
+
+
